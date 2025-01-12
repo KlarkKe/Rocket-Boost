561030dd3c927810440bdaf7c69f37bcd2de003b
KlarkKe <tamnends@gmail.com>
2025-01-13 01:36:18 +0300

    Rigidbody Tuning

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -43,7 +43,9 @@ public class Movement : MonoBehaviour
 
     private void ApplyRotation(float rotationThisFrame)
     {
+        rb.freezeRotation = true;
         transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
+        rb.freezeRotation = false;
     }
 
     private void ProcessThrust()
